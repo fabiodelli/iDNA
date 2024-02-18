f0cd3a83e8893e6355e55a874079e4c45ac8eb4b
fabiodelli <ntqdel@hotmail.com>
2024-02-18 23:39:35 +0100

    add footer text

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,1 +1,21 @@
-<div class="footer">ciao</div>
+<div class="footer">
+
+    <div class="text-white footer_link">
+
+        <span class="me-5">
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> ABOUT</div>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> BOARDS</div>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> ACCESSORIES</div>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> BLOG</div>
+        </span>
+    
+        <span>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> TECNOLOGY</div>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> TEAM</div>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> DEALERS</div>
+            <div><i class="fa-solid fa-window-minimize" style="color: #FFD43B;"></i> CONTACT US</div>
+        </span>
+
+    </div>
+
+</div>
